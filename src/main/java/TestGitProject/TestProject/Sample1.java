63feb07779a448548254b746a0abc460024b0471
Anusree <anusreept2104@gmail.com>
2025-05-23 15:10:52 +0530

    Latest code commited

--- src/main/java/TestGitProject/TestProject/Sample1.java
+++ src/main/java/TestGitProject/TestProject/Sample1.java
@@ -9,4 +9,11 @@ public class Sample1 {
 	public void method2() {
 		System.out.println("*******************Method 2************************");
 	}
+	public void method3() {
+		System.out.println("*******************Method 3************************");
+	}public void method4() {
+		System.out.println("*******************Method 4************************");
+	}public void method5() {
+		System.out.println("*******************Method 5************************");
+	}
 }
