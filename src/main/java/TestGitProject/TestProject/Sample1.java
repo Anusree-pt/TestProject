c057d0f83b2c2fd9767b5ddc8833a777950a7636
Anusree <anusreept2104@gmail.com>
2025-05-26 14:18:45 +0530

    Pushed by user A

--- src/main/java/TestGitProject/TestProject/Sample1.java
+++ src/main/java/TestGitProject/TestProject/Sample1.java
@@ -5,13 +5,29 @@ public class Sample1 {
 	public void method1() {
 		System.out.println("*******************Method 1*****************");
 	}
-	
+
 	public void method2() {
 		System.out.println("*******************Method 2************************");
 	}
+
 	public void method3() {
 		System.out.println("*******************Method 3************************");
-	}public void method4() {
+	}
+
+	public void method4() {
 		System.out.println("*******************Method 4************************");
-	
-}}
+	}
+
+	public void method5() {
+		System.out.println("*******************Method 5************************");
+	}
+
+	public void method6() {
+		System.out.println("*******************Method 6************************");
+	}
+
+	public void method7() {
+		System.out.println("*******************Method 7************************");
+
+	}
+}
