5d94e5186d14dba55f63b4dec7ce66a3caf56972
Anusree <anusreept2104@gmail.com>
2025-05-28 12:10:01 +0530

    commit 1

--- src/main/java/TestGitProject/TestProject/Sample1.java
+++ src/main/java/TestGitProject/TestProject/Sample1.java
@@ -3,31 +3,31 @@ package TestGitProject.TestProject;
 public class Sample1 {
 
 	public void method1() {
-		System.out.println("*******************Method 1*****************");
+		System.out.println("*******************Method a*****************");
 	}
 
 	public void method2() {
-		System.out.println("*******************Method 2************************");
+		System.out.println("*******************Method b************************");
 	}
 
 	public void method3() {
-		System.out.println("*******************Method 3************************");
+		System.out.println("*******************Method c************************");
 	}
 
 	public void method4() {
-		System.out.println("*******************Method 4************************");
+		System.out.println("*******************Method d************************");
 	}
 
 	public void method5() {
-		System.out.println("*******************Method 5************************");
+		System.out.println("*******************Method e************************");
 	}
 
 	public void method6() {
-		System.out.println("*******************Method 6************************");
+		System.out.println("*******************Method f************************");
 	}
 
 	public void method7() {
-		System.out.println("*******************Method 7************************");
+		System.out.println("*******************Method g************************");
 
 	}
 
